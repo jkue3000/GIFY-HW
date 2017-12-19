56d68ed25255e590c5dc625d25718bcce5435d08
unknown <jkue3000@gmail.com>
2017-12-19 15:27:01 -0500

    updated HTML and JS

--- assets/javascript/gify.js
+++ assets/javascript/gify.js
@@ -7,33 +7,45 @@ var apiKey = "KjGZz4rr3h1EYc0PtGyiMVW9DXYclcWO";
 var urlQuery = "https://api.giphy.com/v1/gifs/search?q=";
 
 
-
+// function to make buttons
+function btnCreation() {
+	// for loop function to create a button for each 
+	for (i=0; i<failArray.length; i++){
+		var addSearchBtn = $('<button id="failBtn' + i + '">');
+		$('.holdBtn').append(addSearchBtn);
+	}
+};
 
 
 // on click BTN listener to add buttons 
 $('#addFail').on('click', function(){
 	// var to capture userinput value
-	var userInput = $('#userSearchInput').val();
+	var userInput = $('#userSearchInput').val().trim();
 	
 	// function to push userInput into failArray
 	function pushUserInput(){
-	failArray.push(userInput);
+		failArray.push(userInput);
 	};
 	
 	// creating the userInput value to be able to use it as a string
 	$("userSearchInput").append('value=' + userInput);
-	pushUserInput();
+
+	// conditional to push user input into array or check if a string
+	if (userInput == ""){
+		alert("Oops... Search a Fail!");
+	} else{
+		pushUserInput();
+		$('#userSearchInput').val("");
+	}
+
 	console.log(failArray);
-
-	// for loop to create a button for each 
-
-
 
 	return false;
 
 });
 
-console.log(failArray);
+
+
 
 
 
